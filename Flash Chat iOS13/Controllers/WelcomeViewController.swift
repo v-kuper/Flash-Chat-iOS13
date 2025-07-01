d3b83499ddecaa82fc2bbe16587d8e75993a0b4c
v-kuper <vkuper7@gmail.com>
2025-07-01 22:38:33 +0300

    feat: add print text animation

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        titleLabel.text = ""
+        let titleText = "⚡️FlashChat"
+        
+        for (index, letter) in titleText.enumerated() {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(index), repeats: false) { _ in
+                self.titleLabel.text?.append(letter)
+            }
+        }
     }
     
-
 }
