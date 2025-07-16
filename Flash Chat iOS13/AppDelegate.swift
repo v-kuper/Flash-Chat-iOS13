1cae809c1ec74685f8bac42746a728db7f0ebc06
v-kuper <vkuper7@gmail.com>
2025-07-16 23:23:51 +0300

    feat: add firestore db

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import FirebaseCore
-import FirebaseFirestore
 import FirebaseAuth
 
 @UIApplicationMain
--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    
+    let db = Firestore.firestore()
     
     var messages: [Message] = [
         Message(sender: "1@2.com", body: "Hello"),
@@ -32,7 +35,18 @@ class ChatViewController: UIViewController {
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
-        
+        if let messageBady = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField:messageSender,
+                K.FStore.bodyField:messageBady
+            ]) { (error) in
+                if let e = error {
+                    print("There was an issue saving data to firestore: \(e)")
+                } else {
+                    print("Successfully saved data.")
+                }
+            }
+        }
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
