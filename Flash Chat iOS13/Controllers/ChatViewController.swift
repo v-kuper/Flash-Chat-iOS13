b92ebf9c0d3bd946c4c76ede5e616645d4c3c0bf
v-kuper <vkuper7@gmail.com>
2025-07-20 17:53:31 +0300

    feat: add  auto layout MessageCell

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -27,7 +27,7 @@ class ChatViewController: UIViewController {
         title = K.appName
         navigationItem.hidesBackButton = true
         
-        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        tableView.register(MessageCell.self, forCellReuseIdentifier: K.cellIdentifier)
         
         loadMessages()
     }
@@ -59,6 +59,8 @@ class ChatViewController: UIViewController {
 
                     DispatchQueue.main.async {
                         self.tableView.reloadData()
+                        let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                        self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                     }
                 }
             }
@@ -76,7 +78,9 @@ class ChatViewController: UIViewController {
                     print("There was an issue saving data to firestore: \(e)")
                 } else {
                     print("Successfully saved data.")
-                    self.messageTextfield.text = ""
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text = ""
+                    }
                 }
             }
         }
@@ -98,8 +102,10 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
-        cell.label?.text = messages[indexPath.row].body
+        let currentUserEmail = Auth.auth().currentUser?.email
+        cell.configure(for: message, isCurrentUser: message.sender == currentUserEmail)
         return cell
     }
 }
--- Flash Chat iOS13/Views/MessageCell.swift
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -2,28 +2,98 @@
 //  MessageCell.swift
 //  Flash Chat iOS13
 //
-//  Created by Vitali Kupratsevich on 14.07.25.
+//  Created by Vitali Kupratsevich on 20.07.25.
 //  Copyright © 2025 Angela Yu. All rights reserved.
 //
-
 import UIKit
 
 class MessageCell: UITableViewCell {
+    let messageBubble = UIView()
+    let label = UILabel()
+    let leftImageView = UIImageView()
+    let rightImageView = UIImageView()
+    private var bubbleLeading: NSLayoutConstraint?
+    private var bubbleTrailing: NSLayoutConstraint?
 
-    @IBOutlet weak var messageBubble: UIView!
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var rightImageView: UIImageView!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        setupViews()
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupViews() {
+        contentView.addSubview(leftImageView)
+        contentView.addSubview(rightImageView)
+        contentView.addSubview(messageBubble)
+        messageBubble.addSubview(label)
+
+        messageBubble.layer.cornerRadius = 15
+        messageBubble.clipsToBounds = true
+
+        label.numberOfLines = 0
+
+        leftImageView.translatesAutoresizingMaskIntoConstraints = false
+        rightImageView.translatesAutoresizingMaskIntoConstraints = false
+        messageBubble.translatesAutoresizingMaskIntoConstraints = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        leftImageView.image = UIImage(named: "YouAvatar")
+        rightImageView.image = UIImage(named: "MeAvatar")
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            leftImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            leftImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            leftImageView.widthAnchor.constraint(equalToConstant: 40),
+            leftImageView.heightAnchor.constraint(equalToConstant: 40),
+
+            rightImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            rightImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            rightImageView.widthAnchor.constraint(equalToConstant: 40),
+            rightImageView.heightAnchor.constraint(equalToConstant: 40),
+
+            messageBubble.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            messageBubble.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            messageBubble.widthAnchor.constraint(lessThanOrEqualToConstant: 250),
+
+            label.topAnchor.constraint(equalTo: messageBubble.topAnchor, constant: 10),
+            label.bottomAnchor.constraint(equalTo: messageBubble.bottomAnchor, constant: -10),
+            label.leadingAnchor.constraint(equalTo: messageBubble.leadingAnchor, constant: 10),
+            label.trailingAnchor.constraint(equalTo: messageBubble.trailingAnchor, constant: -10)
+        ])
+
+        // Bubble horizontal constraints for toggling
+        bubbleLeading = messageBubble.leadingAnchor.constraint(equalTo: leftImageView.trailingAnchor, constant: 10)
+        bubbleTrailing = messageBubble.trailingAnchor.constraint(equalTo: rightImageView.leadingAnchor, constant: -10)
+        // Set both inactive initially
+        bubbleLeading?.isActive = false
+        bubbleTrailing?.isActive = false
+    }
+
+    func configure(for message: Message, isCurrentUser: Bool) {
+        label.text = message.body
+
+        leftImageView.isHidden = isCurrentUser
+        rightImageView.isHidden = !isCurrentUser
+
+        // Toggle bubble constraints
+        bubbleLeading?.isActive = false
+        bubbleTrailing?.isActive = false
         
-        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+        if isCurrentUser {
+            messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            label.textColor = UIColor(named: K.BrandColors.purple)
+            bubbleTrailing?.isActive = true
+        } else {
+            messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            label.textColor = UIColor(named: K.BrandColors.lightPurple)
+            bubbleLeading?.isActive = true
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
